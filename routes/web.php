02a252d8191322429b86d2089fdc399a31a977b8
ivangeorgiev97 <georgiev1313@abv.bg>
2018-03-09 23:20:10 +0200

    Add route groups

--- routes/web.php
+++ routes/web.php
@@ -12,53 +12,41 @@
 */
 
 Route::get('/', 'HomeController@index');
-
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/categories/{id}', 'ProductsByCategoryController@getProductsFromCategroy');
-
 Route::get('/product/{id}', 'CurrentProductController@getCurrentProduct');
 
 // ORDERS
 
 Route::get('/order_details','OrdersController@index');
-
 Route::post('/makeOrder','OrdersController@makeOrder');
-
-// CART
-
-Route::post('addToCart','CartController@create')->middleware('auth');
-
-Route::get('myCart','CartController@index')->middleware('auth');
-
-Route::get('removeCartItem/{rowId}','CartController@removeItem')->middleware('auth');
-        
 
 // SEARCH 
 
 Route::get('search/autocomplete', ['as' => 'search-autocomplete', 'uses' => 'SearchController@autocomplete']);
-
 Route::get('search/searchresults', 'SearchController@getSearchResult');
+Route::group(['middleware' => 'auth'], function(){
 
-
-// ADMIN CP - EVERYTHING SHOULD BE ON SEPARATE CONTROLLER
+// CART
+Route::post('addToCart','CartController@create')->middleware('auth');
+Route::get('myCart','CartController@index')->middleware('auth');
+Route::get('removeCartItem/{rowId}','CartController@removeItem')->middleware('auth');
+      
+// AdminCP 
+Route::group(['middleware' => 'admin'], function(){
 
 Route::get('/admincp','AdminController@index')->middleware('auth')->middleware('admin');
-
 Route::get('/admincp/orders','AdminController@getOrders')->middleware('auth')->middleware('admin');
-
-Route::post('/admincp/orderSent','AdminController@orderNotSent')->middleware('auth')->middleware('admin'); // 
-
-Route::post('/admincp/orderNotSent','AdminController@orderSent')->middleware('auth')->middleware('admin'); //
-
+Route::post('/admincp/orderSent','AdminController@orderNotSent')->middleware('auth')->middleware('admin'); 
+Route::post('/admincp/orderNotSent','AdminController@orderSent')->middleware('auth')->middleware('admin'); 
 Route::get('/admincp/products','AdminController@getProducts')->middleware('auth')->middleware('admin');
-
 Route::get('/admincp/addProduct','AdminController@getAddProduct')->middleware('auth')->middleware('admin');
-
 Route::post('/admincp/addProduct','AdminController@addProduct')->middleware('auth')->middleware('admin');
-
 Route::get('/admincp/categories','AdminController@getCategories')->middleware('auth')->middleware('admin');
 
+});
+
+});
 
 // Auth
 
