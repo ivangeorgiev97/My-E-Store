ab388c021f136a1251fa0a630b602a6fcf5c0686
ivangeorgiev97 <georgiev1313@abv.bg>
2018-03-18 20:16:55 +0200

    Update Search Function

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,26 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Response;
+use View;
 use App\Product;
+use App\Category;
 
 class SearchController extends Controller
 {
     public function autocomplete() {
 	$term = request('term');
         $result = Product::where('product_name', 'LIKE', '%' . $term . '%')->get(['id', 'product_name as value']);
-        return response()->json($result);//term
-      
+          
+        return response()->json($result);
     }
     
-    public function getSearchResult(Request $request) {
+    public function getSearchResult(Request $request) { 
+        if (!empty($request->term)) {
         $term = $request->term;
-        $result = Product::where('product_name', 'LIKE', '%' . $term . '%')->get(['id']);
-        $id;
-        foreach($result as $res) {
-            $id = $res['id'];
+        $products = Product::where('product_name', 'LIKE', '%' . $term . '%')->paginate(8); //->get(['id']);
+        $categories = Category::all();
+        // $id;
+//        foreach($result as $res) {
+//            $id = $res['id'];
+//        }
+        
+        return View::make('productsByCategory')
+                    ->with('products',$products)
+                    ->with('categories',$categories);
+        
+       // return redirect('/product/'.$id);
         }
         
-        return redirect('/product/'.$id);
+        return redirect()->route('home');
     }
 
 }
---
+++ resources/views/searchResults.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container-fluid">
+
+    <div class="row"  id="firstRowIndex">
+        
+            @include('shared.leftSidebar')
+
+            <div class="col-md-offset-1 col-md-8" id="rightContent">
+                <section>
+                    <h4>Products:</h4>
+                     <hr class="hr">
+                      @foreach($products as $product)
+                      <div class="divImages col-sm-3">
+                          <a href="{{route('product.show',$product['id'])}}"><img class="homeImages img-thumbnail" src="{{ asset('images') }}/{{$product['product_img']}}"></a>
+                          <p><a href="{{route('product.show',$product['id'])}}">{{$product['product_name']}} | Click to view more details</a></p>
+                          <p>Price: <b style="font-family: Arial;">{{$product['product_price']}} BGN</b></p>
+    
+                          
+                      </div>
+                      @endforeach
+                      <br>
+                      <div class="container-fluid"><div class="row"><div id="productsPagination" class="col-md-12">{{ $products->links() }}</div></div></div>
+                </section>
+            </div>
+    
+    </div>
+</div>
+
+@endsection
